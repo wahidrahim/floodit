d3065a5552d212887c7d387566282f4b139e49e2
Wahid Rahim <wahidrahim@gmail.com>
2020-01-07 17:00:58 -0500

    using ts-node to run webpack-dev-server

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-  render() {
-    return <h1>It's working!</h1>
-  }
-}
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -3,18 +3,27 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 
 const config: webpack.Configuration = {
-  entry: 'client/index.tsx',
+  entry: path.resolve(__dirname, 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin({ template: 'client/public/index.html' })],
-  // resolve: {
-  //   modules: [__dirname, 'node_modules'],
-  //   extensions: ['.ts', '.tsx', '.js']
-  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html')
+    })
+  ],
+  resolve: {
+    // modules: [__dirname, 'node_modules'],
+    extensions: ['.ts', '.tsx', '.js']
+  },
   module: {
-    rules: [{ test: /\.ts|\.tsx$/, use: 'ts-loader' }]
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader'
+      }
+    ]
   }
 }
 
