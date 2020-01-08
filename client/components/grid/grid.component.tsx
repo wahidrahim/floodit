2db97aa6ae78455f3c0e2785e2269e108ca8b543
Wahid Rahim <wahidrahim@gmail.com>
2020-01-08 14:49:43 -0500

    gridifying cells

--- client/components/grid/grid.component.tsx
+++ client/components/grid/grid.component.tsx
@@ -2,9 +2,42 @@ import React from 'react'
 
 import './grid.styles.scss'
 
-class Grid extends React.Component {
+interface GridProps {
+  rows?: number
+  cols?: number
+}
+
+class Grid extends React.Component<GridProps> {
+  width = 200
+  height = 200
+  gridCellSizes = ''
+
+  constructor(props: GridProps) {
+    super(props)
+
+    // TODO: ensure children must be present, to avoid nullish coellescing, and not-null identifiers
+    const numberOfChildren = this.props.children?.valueOf().length // Children array length
+    const CellSize = 100 / (numberOfChildren! / 2)
+
+    for (let i = 0; i < numberOfChildren! / 2; i++) {
+      this.gridCellSizes += `${CellSize}%`
+    }
+  }
+
   render() {
-    return <div className="grid">{this.props.children}</div>
+    return (
+      <div
+        className="grid"
+        style={{
+          width: this.width,
+          height: this.height,
+          gridTemplateColumns: this.gridCellSizes,
+          gridTemplateRows: this.gridCellSizes
+        }}
+      >
+        {this.props.children}
+      </div>
+    )
   }
 }
 
